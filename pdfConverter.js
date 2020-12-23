48b163b484a3a54a89b54e4a6a5c4f13fa7f9e8f
Erdem Camlioglu <secamlioglu@gmail.com>
2020-12-23 20:31:29 +0300

    modules imported and about page started

--- pdfConverter.js
+++ pdfConverter.js
@@ -1,2 +1,13 @@
-const markdownpdf = require('markdown-pdf');
+const markdownEditor = document.querySelector(".text-editor");
+const markdownPreview = document.querySelector(".preview");
+//const converter = new showdown.Converter();
 
+const mdConverter = new showdown.Converter();
+ 
+
+
+markdownEditor.addEventListener("keyup", evt => {
+    const {value} = evt.target;
+    const markdownForHtml = converter.makeHtml(value);
+    markdownPreview.innerHTML = markdownForHtml;
+});
